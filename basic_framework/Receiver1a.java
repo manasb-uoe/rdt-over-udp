b653da55898cee780be36ed1d1b42e8c73af22d5
Manas Bajaj <manas.bajaj94@gmail.com>
2015-02-08 17:52:15 +0000

    Added receiver and sender files for 'basic framework' and 'stop and wait'

---
+++ basic_framework/Receiver1a.java
@@ -0,0 +1,119 @@
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+public class Receiver1a {
+
+    private final String TAG = "[" + Receiver1a.class.getSimpleName() + "]";
+    private DatagramSocket serverSocket;    
+
+    public Receiver1a(int port) {
+        try {
+            serverSocket = new DatagramSocket(port);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void receiveFile(String filePath) {
+        ByteArrayOutputStream baos = null;
+
+        try {
+            baos = new ByteArrayOutputStream();
+
+            int sequenceNum = 0;
+            boolean isLastPacket = false;
+
+            while (!isLastPacket) {
+                // create separate byte arrays for full packet bytes and file bytes (without header)
+                byte[] packetBytes = new byte[1024];
+                byte[] fileBytes = new byte[1021];
+
+                // receive packet and retrieve file and header bytes from it
+                DatagramPacket receivedPacket = new DatagramPacket(packetBytes, packetBytes.length);
+                serverSocket.receive(receivedPacket);
+
+                // retrieve sequence number from header
+                sequenceNum = ((packetBytes[0] & 0xFF) << 8) + (packetBytes[1] & 0xFF);
+
+                // retrieve last message flag
+                isLastPacket = (packetBytes[2] & 0xFF) == 1;
+
+                // retrieve file bytes from packet bytes
+                for (int i=3; i<packetBytes.length; i++) {
+                    fileBytes[i-3] = packetBytes[i];
+                }
+
+                // write file bytes to file
+                baos.write(fileBytes);
+
+                System.out.println("Sent: Sequence number = " + sequenceNum + ", Flag = " + isLastPacket);
+
+                // if last packet reached, write all bytes to file and quit
+                if (isLastPacket) {
+                    serverSocket.close();
+                    writeToFile(filePath, baos.toByteArray());
+                    baos.close();
+                    break;
+                }
+            }
+
+            System.out.println(TAG + " File received and saved successfully");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (serverSocket != null) {
+                serverSocket.close();
+            }
+            if (baos != null) {
+                try {
+                    baos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    
+    public static void writeToFile(String filePath, byte[] bytes) {
+        File file = new File(filePath);
+        if (file.exists()) {
+            file.delete();
+        }
+
+        FileOutputStream fos = null;
+
+        try {
+            fos = new FileOutputStream(file);
+            fos.write(bytes);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (fos != null) {
+                    fos.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(final String[] args) {
+        Thread serverThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Receiver1a server = new Receiver1a(Integer.parseInt(args[0]));
+                server.receiveFile(args[1]);
+            }
+        });
+        serverThread.start();
+
+    }
+}
---
+++ basic_framework/Sender1a.java
@@ -0,0 +1,126 @@
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+public class Sender1a {
+
+    private final String TAG = "[" + Sender1a.class.getSimpleName() + "]";  
+    private DatagramSocket clientSocket;
+
+    public Sender1a() {
+        try {
+            clientSocket = new DatagramSocket();
+
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendFile(String destServerName, int destPort, String filePath) {
+        System.out.println(TAG + " Started sending file");
+
+        try {
+            // get ip address of destination
+            InetAddress destIPAddress = InetAddress.getByName(destServerName);
+
+            // read file into a byte array
+            File fileToSend = new File(filePath);
+            byte[] fileBytes = getBytesFromFile(fileToSend);
+
+            // divide file bytes into smaller packets of size 1024 bytes
+            // and then send each of these packets to destination
+            int sequenceNum = 0;
+            boolean isLastPacket = false;
+
+            for (int i=0; i<fileBytes.length; i+=1021) {
+                byte[] packetBytes = new byte[1024];
+
+                // add 16 bit sequence number as first 2 bytes
+                packetBytes[0] = (byte) (sequenceNum >> 8);
+                packetBytes[1] = (byte) (sequenceNum);
+
+                // add last message flag as 3rd byte
+                isLastPacket = i+1021 >= fileBytes.length;
+                packetBytes[2] = isLastPacket ? (byte) 1 : (byte) 0;
+
+                // add file bytes to remaining 1021 bytes
+                if (!isLastPacket) {
+                    for (int j=0; j<1021; j++) {
+                        packetBytes[j+3] = fileBytes[i+j];
+                    }
+                }
+                else {
+                    // if last packet, only write remaining bytes instead of 1021
+                    for (int j=0; j<fileBytes.length-i; j++) {
+                        packetBytes[j+3] = fileBytes[i+j];
+                    }
+                }
+
+                // finally, send the packet to destination
+                DatagramPacket packetToSend = new DatagramPacket(packetBytes, packetBytes.length, destIPAddress, destPort);
+                clientSocket.send(packetToSend);
+
+                // Sleep for 10ms between sending packets to prevent losing packets
+                Thread.sleep(50);
+
+                sequenceNum++;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (clientSocket != null) {
+                clientSocket.close();
+            }
+        }
+    }
+
+    public static byte[] getBytesFromFile(File file) {
+        byte[] buffer = new byte[4096];
+        InputStream is = null;
+        ByteArrayOutputStream baos = null;
+
+        try {
+            is = new FileInputStream(file);
+            baos = new ByteArrayOutputStream((int) file.length());
+
+            int read = 0;
+            while ((read = is.read(buffer)) != -1) {
+                baos.write(buffer, 0, read);
+            }
+
+            return baos.toByteArray();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (baos != null) {
+                try {
+                    baos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Sender1a sender = new Sender1a();
+        sender.sendFile(args[0], Integer.parseInt(args[1]), args[2]);
+    }
+}
